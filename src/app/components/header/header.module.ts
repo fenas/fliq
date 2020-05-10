4c6533014f8c62c865843610c5ba0a9bf3eefa9a
fenas <fenas.fnz@gmail.com>
2020-05-10 15:44:46 +0530

    header and footer added

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
-
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatToolbarModule],
 })
-export class HeaderModule { }
+export class HeaderModule {}
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,15 +1,12 @@
+import { FooterComponent } from './../../components/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, DashboardRoutingModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
